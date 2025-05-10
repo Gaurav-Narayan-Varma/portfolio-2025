a17d35d1316e221d1b5ecbdeac89dc0ea56b1af3
Gaurav-Narayan-Varma <gaurav.varma@hey.com>
2025-05-10 00:21:29 -0700

    feat: resume button

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -44,35 +44,52 @@ export default function Sidebar() {
   const pathname = usePathname();
 
   return (
-    <div className="flex flex-col gap-9 h-screen w-[260px] border-r border-ds-gray-300 pt-6 px-4.5">
-      <UserProfileCard />
+    <div className="flex flex-col justify-between h-screen w-[260px] border-r border-ds-gray-300 pt-6 pb-5 px-4.5">
+      <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-9">
+          <UserProfileCard />
 
-      {/* Main Navigation */}
-      <div className="flex flex-col gap-1">
-        {mainNavItems.map((item) => (
-          <NavItem
-            key={item.href}
-            {...item}
-            isActive={pathname === item.href}
-          />
-        ))}
+          {/* Main Navigation */}
+          <div className="flex flex-col gap-1">
+            {mainNavItems.map((item) => (
+              <NavItem
+                key={item.href}
+                {...item}
+                isActive={pathname === item.href}
+              />
+            ))}
+          </div>
+        </div>
+
+        {/* Connect Navigation */}
+        <div className="flex flex-col gap-4">
+          <div className="text-[13px] text-white/40 font-medium -tracking-[0.26px] leading-[13px] pl-2.5">
+            Connect
+          </div>
+          <div className="flex flex-col gap-1">
+            {connectNavItems.map((item) => (
+              <NavItem
+                key={item.href}
+                {...item}
+                isActive={pathname === item.href}
+                isExternalLink={item.href.startsWith("http")}
+              />
+            ))}
+          </div>
+        </div>
       </div>
 
-      {/* Connect Navigation */}
-      <div className="flex flex-col gap-4">
-        <div className="text-[13px] text-white/40 font-medium -tracking-[0.26px] leading-[13px] pl-2.5">
-          Connect
+      {/* Resume Download */}
+      <a
+        href="/Gaurav-Varma-Resume-2025.pdf"
+        download
+        className="bg-ds-gray-500 cursor-default border border-white/[15%] rounded-[5px] h-10.5 flex items-center justify-center gap-2 opacity-80 hover:opacity-100 hover:bg-ds-gray-600 transition-all duration-150 shadow-[inset_0px_0.557334px_1.672px_-1.125px_rgba(255,255,255,0.07),_inset_0px_1.69015px_5.07046px_-2.25px_rgba(255,255,255,0.063),_inset_0px_4.46786px_13.4036px_-3.375px_rgba(255,255,255,0.05),_inset_0px_14px_42px_-4.5px_rgba(255,255,255,0.008)]"
+      >
+        <img src="/icons/download.svg" alt="Download" className="h-5 w-5" />
+        <div className="-tracking-[0.14px] leading-[21px] font-medium text-sm">
+          Resume
         </div>
-        <div className="flex flex-col gap-1">
-          {connectNavItems.map((item) => (
-            <NavItem
-              key={item.href}
-              {...item}
-              isActive={pathname === item.href}
-            />
-          ))}
-        </div>
-      </div>
+      </a>
     </div>
   );
 }
--- src/components/sidebar/nav-item.tsx
+++ src/components/sidebar/nav-item.tsx
@@ -6,26 +6,38 @@ export default function NavItem({
   icon,
   label,
   isActive,
+  isExternalLink,
 }: {
   href: string;
   icon: string;
   label: string;
   isActive: boolean;
+  isExternalLink?: boolean;
 }) {
   return (
     <Link
       href={href}
+      target={isExternalLink ? "_blank" : undefined}
       className={clsx(
-        "flex gap-2 p-2 cursor-default rounded-md  border border-transparent ",
+        "flex gap-2 p-2 cursor-default rounded-md  border border-transparent h-10 group",
         isActive
-          ? "bg-ds-gray-200 border !border-ds-gray-400 opacity-90"
-          : "opacity-60 hover:bg-ds-gray-100/60 hover:opacity-90"
+          ? "bg-ds-gray-200 border !border-ds-gray-400 opacity-90 transition-all duration-150"
+          : "opacity-60 hover:bg-ds-gray-100 hover:opacity-90"
       )}
     >
       <img src={`/icons/${icon}`} alt={label} width={16} height={16} />
       <div className="font-medium -tracking-[0.14px] leading-[21px] text-sm text-white">
         {label}
       </div>
+      {isExternalLink && (
+        <img
+          src="/icons/external.svg"
+          alt="External Link"
+          className="ml-auto invisible group-hover:visible"
+          width={16}
+          height={16}
+        />
+      )}
     </Link>
   );
 }
